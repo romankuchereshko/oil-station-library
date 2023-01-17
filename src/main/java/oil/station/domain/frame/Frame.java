92bdf199722919a42e3a1db08b2cc8986f27af1c
Roman Kuchereshko <romankuc@ext.inditex.com>
2023-01-18 01:18:26 +0200

    add constructors to Frame

--- src/main/java/oil/station/domain/frame/Frame.java
+++ src/main/java/oil/station/domain/frame/Frame.java
@@ -7,10 +7,14 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder(toBuilder = true)
 public class Frame implements Serializable {
 
